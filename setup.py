660526fbb8de964fa3a84b76f609f03d60ac0c9b
rachanakondabala <rachana11k@gmail.com>
2021-08-31 19:29:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='SEGroup39',
+    version='0.1',
+    description='Module for the SE project',
+    author='SE-Group39',
+    packages=['SEGroup39'],
+      long_description="""\ To be updated """,
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: GITS Project",
+      ],
+      license='MIT',
+      
+      ],
+      )
 
